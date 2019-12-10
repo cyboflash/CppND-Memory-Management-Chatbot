781543194998206a53471583517058bd056d696c
cyboflash <2373916+cyboflash@users.noreply.github.com>
2019-12-09 23:50:22 -0800

    Remove commented out code

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -25,7 +25,6 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-// void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
 void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
     _childEdges.emplace_back(std::move(edge));
